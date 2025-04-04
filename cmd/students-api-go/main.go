e553c37ae13d8f1be6dcc0cf1d02c293cd5ed8f5
vinit-jpl <vinit.jpl@gmail.com>
2025-04-04 16:30:52 +0530

    main file setup

--- cmd/students-api-go/main.go
+++ cmd/students-api-go/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/vinit-jpl/students-api-go/internal/config"
+)
 
 func main() {
-	fmt.Println("Welcome to students api")
+
+	// load config
+	cfg := config.MustLoad()
+
+	// database setup
+
+	// setup router
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome To Students API!"))
+	})
+
+	// setup server
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	fmt.Println("Server started")
+
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatalf("failed to start server: %s", err.Error())
+	}
+
 }
