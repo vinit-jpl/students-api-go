153cb4ceaf062aa073ee3ec00c4bd2469400e328
vinit-jpl <vinit.jpl@gmail.com>
2025-04-08 16:50:28 +0530

    typo fix

--- cmd/students-api-go/main.go
+++ cmd/students-api-go/main.go
@@ -54,9 +54,9 @@ func main() {
 
 	slog.Info("shutting down server...")
 
-	ctx, cancle := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 
-	defer cancle()
+	defer cancel()
 
 	if err := server.Shutdown(ctx); err != nil {
 		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
