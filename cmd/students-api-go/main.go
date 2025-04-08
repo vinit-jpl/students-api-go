22e8006f32d08fd0a7f6d935a30a86fb6c2e5c9c
vinit-jpl <vinit.jpl@gmail.com>
2025-04-08 12:40:45 +0530

    graceful shutdown

--- cmd/students-api-go/main.go
+++ cmd/students-api-go/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/vinit-jpl/students-api-go/internal/config"
 )
@@ -23,15 +28,40 @@ func main() {
 
 	// setup server
 	server := http.Server{
-		Addr:    cfg.Addr,
+		Addr:    cfg.HTTPServer.Addr,
 		Handler: router,
 	}
 
-	fmt.Println("Server started")
+	slog.Info("Server started", slog.String("address", cfg.HTTPServer.Addr))
 
-	err := server.ListenAndServe()
-	if err != nil {
-		log.Fatalf("failed to start server: %s", err.Error())
+	done := make(chan os.Signal, 1)                                    // channel to receive os signal
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM) // notify the channel when os interrupt signal is received
+
+	// creating go routine to run the server in the background
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatalf("failed to start server: %s", err.Error())
+		}
+	}()
+
+	<-done
+	// fmt.Printf("Server started %s", cfg.HTTPServer.Addr)
+	// err := server.ListenAndServe()
+	// if err != nil {
+	// 	log.Fatalf("failed to start server: %s", err.Error())
+	// }
+
+	slog.Info("shutting down server...")
+
+	ctx, cancle := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancle()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
 	}
+
+	slog.Info("Server shutdown gracefully")
 
 }
